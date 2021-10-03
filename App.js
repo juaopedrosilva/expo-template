7dfb4b79231fc7fb1e1d59000f6138929e60e072
João Pedro Silva <pedrojoaoti@gmail.com>
2021-10-02 23:03:25 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar'
+import 'react-native-gesture-handler'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { ThemeProvider } from 'styled-components/native'
+import AppLoading from 'expo-app-loading'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { StatusBar } from 'react-native'
+
+import { Routes } from './src/routes'
+
+import theme from './src/global/styles/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>👊 App Energia Solar 👊</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar
+        backgroundColor={theme.colors.headerColor}
+        barStyle="Visible"
+      />
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
+    </>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',git init
-})
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
-  api.cache(true);
+/* ./babel.config.js */
+/* eslint-disable */
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@components': './src/components',
+            '@global': './src/global',
+            '@pages': './src/pages',
+          },
+        },
+      ],
+    ],
+  }
+}
